2ce30bf9406e68f2562aed391dd20a28f7beabbe
Admin <Admin@home-pc>
2017-03-24 02:06:24 +0200

    Combine exceptions with same handlers and rewrite relative path for file to read

--- src/ua/dp/ort/OOP_16/Main.java
+++ src/ua/dp/ort/OOP_16/Main.java
@@ -2,10 +2,14 @@ package ua.dp.ort.OOP_16;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.SAXException;
+
+import ua.dp.ort.OOP_19_Adv_Ep3_1.ReadWriteFileCalculator;
   
 /**
  * This class run SAXParser, which is read and parse data from XML-file,
@@ -23,16 +27,14 @@ public class Main {
 		SAXParserFactory spf = SAXParserFactory.newInstance();
 		try {
 			SAXParser sp = spf.newSAXParser();
-			sp.parse(new File("src/ua/dp/ort/OOP_16/_01_students.xml"), studentsHandler);
+			sp.parse(new File
+				(Main.class.getResource("_01_students.xml").toURI()), studentsHandler);
 		
-		} catch (ParserConfigurationException e) {
+		} catch (ParserConfigurationException | URISyntaxException e) {
 			e.printStackTrace();
-		} catch (SAXException e) {
+		} catch (SAXException | IOException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+		} 
 		PrintStudentToConsol.printStudentArray(studentsHandler.getStudentArray());
 	}
 	
